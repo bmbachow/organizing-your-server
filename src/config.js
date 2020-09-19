2439c8da11fc9cda7d6c87199ad45b8a5d802f60
Brian Bachow <bmbachow@gmail.com>
2020-09-18 18:46:08 -0700

    finished

--- src/config.js
+++ src/config.js
@@ -1,6 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  API_TOKEN: process.env.API_TOKEN || 'dummy-api-token',
-  DB_URL: process.env.DB_URL || 'postgresql://dunder-mifflin@localhost/bookmarks',
+  API_TOKEN: process.env.API_TOKEN || '6033df03-bea5-418f-b4c2-882782118b3a',
 }
